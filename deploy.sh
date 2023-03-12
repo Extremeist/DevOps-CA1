d76c0ce6b25a0ca56aee7142befbf44d8d35b9c2
Lloyd Portes <56543588+Extremeist@users.noreply.github.com>
2023-03-12 13:31:55 +0000

    changed deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 sudo apt update && sudo apt install nodejs npm
 sudo npm install -g pm2
 pm2 stop DevOps-CA1
-cd DevOps-CA1/
+cd SimpleApplication/
 npm install
 pm2 start ./bin/www --name DevOps-CA1 --env=production
