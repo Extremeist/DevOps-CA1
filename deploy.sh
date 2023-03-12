0081d5d37fa376cc45f554451b7cf324b341a064
Lloyd Portes <56543588+Extremeist@users.noreply.github.com>
2023-03-12 16:00:26 +0000

    updated pm2 in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,11 @@
 sudo apt update && sudo apt install nodejs npm
 sudo npm install -g pm2
 pm2 stop DevOps-CA1
-cd SimpleApplication/
+cd SimpleApplication
 npm install
+
+# Restart the application using pm2
+pm2 stop app || true
+pm2 start app.js --name app
+
 pm2 start ./bin/www --name DevOps-CA1 --env=production
