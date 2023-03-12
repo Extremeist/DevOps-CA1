c157bea371d497f7b3db432eb68fb8fd567a6aed
Lloyd Portes <56543588+Extremeist@users.noreply.github.com>
2023-03-12 13:33:46 +0000

    changed deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 sudo apt update && sudo apt install nodejs npm
+# Install pm2 which is a production process manager for Node.js with a built-in load balancer
 sudo npm install -g pm2
-pm2 stop DevOps-CA1
+# stop any instance of our application running currently
+pm2 stop sample_app
+# change directory into folder where application is downloaded
 cd SimpleApplication/
+# Install application dependencies
 npm install
-pm2 start ./bin/www --name DevOps-CA1 --env=production
+
+# Start the application with the process name example app using pm2
+pm2 start ./bin/www --name sample_app
